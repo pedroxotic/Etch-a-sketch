826f4fcf5278587dce4266ef8ae9d74b7362af5e
Pedro <465.morales.pedro@gmail.com>
2023-04-16 19:10:15 -0500

    button added, user can now enter the desired grid size

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const container= document.querySelector('.container');
 // creating the 256 divs:
 for (let index = 0; index < 16; index++) {
-    const row = document.createElement('div'); row.classList.add('row');
+    var row = document.createElement('div'); row.classList.add('row');
     container.appendChild(row);
     for (let index = 0; index < 16; index++) {
         const square = document.createElement('div');
@@ -14,6 +14,40 @@ for (let index = 0; index < 16; index++) {
 
 const squares=document.querySelectorAll('.square');
 
+
+
+const button=document.querySelector('button');
+
+button.addEventListener('click', ()=>{
+    var gridSize=prompt('enter the desired grid size');
+    console.log(gridSize);
+    const rows = document.querySelectorAll('.row');
+    //delete the 16*16
+    rows.forEach(row => {
+    container.removeChild(row);
+  }); 
+  //creating the new grid
+  for (let index = 0; index < gridSize; index++) {
+    var row = document.createElement('div'); row.classList.add('row');
+    container.appendChild(row);
+    for (let index = 0; index < gridSize; index++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+            
+    }
+    const squares=document.querySelectorAll('.square');
+
+    squares.forEach((square)=>{
+
+        square.addEventListener('mouseover', ()=>{
+            square.style.backgroundColor='black';
+    
+        })
+    });
+}
+});
+
 squares.forEach((square)=>{
 
     square.addEventListener('mouseover', ()=>{
@@ -21,4 +55,3 @@ squares.forEach((square)=>{
 
     })
 });
-
