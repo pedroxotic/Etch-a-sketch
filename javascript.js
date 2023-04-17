a56b7bceff2a8552e6004867fdc4a68c373c3d4a
Pedro <465.morales.pedro@gmail.com>
2023-04-16 19:34:27 -0500

    the maximum grid's size is 100 now

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const container= document.querySelector('.container');
-// creating the 256 divs:
+// creating the 16 * 16 grid:
 for (let index = 0; index < 16; index++) {
     var row = document.createElement('div'); row.classList.add('row');
     container.appendChild(row);
@@ -14,40 +14,6 @@ for (let index = 0; index < 16; index++) {
 
 const squares=document.querySelectorAll('.square');
 
-
-
-const button=document.querySelector('button');
-
-button.addEventListener('click', ()=>{
-    var gridSize=prompt('enter the desired grid size');
-    console.log(gridSize);
-    const rows = document.querySelectorAll('.row');
-    //delete the 16*16
-    rows.forEach(row => {
-    container.removeChild(row);
-  }); 
-  //creating the new grid
-  for (let index = 0; index < gridSize; index++) {
-    var row = document.createElement('div'); row.classList.add('row');
-    container.appendChild(row);
-    for (let index = 0; index < gridSize; index++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
-            
-    }
-    const squares=document.querySelectorAll('.square');
-
-    squares.forEach((square)=>{
-
-        square.addEventListener('mouseover', ()=>{
-            square.style.backgroundColor='black';
-    
-        })
-    });
-}
-});
-
 squares.forEach((square)=>{
 
     square.addEventListener('mouseover', ()=>{
@@ -55,3 +21,42 @@ squares.forEach((square)=>{
 
     })
 });
+
+
+const button=document.querySelector('button');
+
+button.addEventListener('click', ()=>{
+    var gridSize=parseInt(prompt('enter the desired grid size'));
+    console.log(gridSize);
+    
+    if (gridSize<=100) {
+        const rows = document.querySelectorAll('.row');
+        //delete the 16*16 grid
+        rows.forEach(row => {
+        container.removeChild(row);
+      }); 
+      
+        //creating the new grid
+     for (let index = 0; index < gridSize; index++) {
+     var row = document.createElement('div'); row.classList.add('row');
+     container.appendChild(row);
+     for (let index = 0; index < gridSize; index++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+        }
+    }
+    const squares=document.querySelectorAll('.square');
+    squares.forEach((square)=>{
+        square.addEventListener('mouseover', ()=>{
+            square.style.backgroundColor='black';
+    
+        })
+    });
+}
+else{
+    alert('the grid cant be more than 100*100 try again');
+}
+
+});
+
